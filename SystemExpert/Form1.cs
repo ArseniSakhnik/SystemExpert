be8325213b279c71ce7c38475033225476c0a0b8
keshe4ka <keshe4ka@users.noreply.github.com>
2021-12-11 22:21:23 +0300

    темыч наколдовал

--- SystemExpert/Form1.cs
+++ SystemExpert/Form1.cs
@@ -15,16 +15,88 @@ namespace SystemExpert
         public Form1()
         {
             InitializeComponent();
+            start_form();
         }
 
-        private void YesBtn_Click(object sender, EventArgs e)
+        /// <summary>
+        /// Функция возвращает свойства элементов к начальной
+        /// </summary>
+        private void start_form()
         {
-            Question.Text = "Вы хохол";
+            rock_button.Visible = true;
+            rap_button.Visible = true;
+            pop_button.Visible = true;
+            rock_button.Enabled = true;
+            rap_button.Enabled = true;
+            pop_button.Enabled = true;
+            retry_button.Visible = false; //кнопка "начать сначала" невидима
+            rock_groupBox.Visible = false;
+            rap_groupBox.Visible = false;
+            pop_groupBox.Visible = false;
         }
 
-        private void NoBtn_Click(object sender, EventArgs e)
+        private void rock_button_Click(object sender, EventArgs e)
         {
-            Question.Text = "Да кому ты пиздишь";
+            start_form();
+            rock_button.Enabled = false; //нельзя снова выбрать рок
+            retry_button.Visible = true;
+            rock_groupBox.Visible = true; //показываем вопросы для РоКа
+        }
+
+        private void rap_button_Click(object sender, EventArgs e)
+        {
+            start_form();
+            rap_button.Enabled = false; //нельзя снова выбрать рэп
+            retry_button.Visible = true;
+            rap_groupBox.Visible = true; //показываем вопросы для РэПа
+        }
+
+        private void pop_button_Click(object sender, EventArgs e)
+        {
+            start_form();
+            pop_button.Enabled = false; //нельзя снова выбрать поп
+            retry_button.Visible = true;
+            pop_groupBox.Visible = true; //показываем вопросы для Жопы
+        }
+
+
+        /// <summary>
+        /// Тупа вызываем start_form
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void retry_button_Click(object sender, EventArgs e)
+        {
+            start_form();
+        }
+
+        /// <summary>
+        /// Радиокнопка "Да" для 1го вопроса
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void question1True_CheckedChanged(object sender, EventArgs e)
+        {
+            metal_groupBox.Visible = true; //показываем групбокс для митола
+        }
+
+        /// <summary>
+        /// Радиокнопка "Нет" для 1го вопроса
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void question1False_CheckedChanged(object sender, EventArgs e)
+        {
+            question1.Visible = false; //скрываем первый вопрос
+            question2.Visible = true; //показываем второй
+
+            //скрываем два радиобокса для 1-го вопроса
+            question1_True.Visible = false; 
+            question1_False.Visible = false;
+
+            //но показываем для 2-го
+            question2_True.Visible = true;
+            question2_False.Visible = true;
         }
     }
 }
--- SystemExpert/Services/GenreService.cs
+++ SystemExpert/Services/GenreService.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SystemExpert.Data;
 using SystemExpert.Entities;
 using SystemExpert.Enums;
 
